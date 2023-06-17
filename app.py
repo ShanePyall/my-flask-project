7409ec5f606617094b9153e3add4c9e841b3cad7
Shane Pyall <shanepyall@gmail.com>
2023-06-17 09:16:09 +0100

    Included: json storage file, html template, css style sheet.

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+import flask
+from flask import render_template
+import json
 
-app = Flask(__name__)
+app = flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # Sends a list of blog posts to a template for display
+    with open("data-structures.json", "r") as readable:
+        blog_posts = json.loads(readable.read())
+    return render_template('index.html', posts=blog_posts) # renders file with value posts replaced with a list.
 
 
 if __name__ == '__main__':
